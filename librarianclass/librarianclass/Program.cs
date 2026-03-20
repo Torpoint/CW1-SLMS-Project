ede555952f453859cc2c20bf49e18bf6220687a9
Owen <123762165+Torpoint@users.noreply.github.com>
2026-03-20 08:59:37 +0000

    Add files via upload

---
+++ librarianclass/librarianclass/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Librarian : User
+{
+    public Librarian(string userID, string name, string password)
+        : base(userID, name, password) { }
+
+    public override string DisplayRole()
+    {
+        return "Librarian";
+    }
+
+    public void AddBook(LibrarySystem system, Book book)
+    {
+        system.AddBook(book);
+        Console.WriteLine("Book added to system.");
+    }
+
+    public void RemoveBook(LibrarySystem system, Book book)
+    {
+        system.RemoveBook(book);
+        Console.WriteLine("Book removed from system.");
+    }
+
+    public void UpdateBook(Book book, string newTitle, string newAuthor)
+    {
+        book.SetTitle(newTitle);
+        book.SetAuthor(newAuthor);
+        Console.WriteLine("Book updated.");
+    }
+}
