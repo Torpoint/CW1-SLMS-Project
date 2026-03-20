bf8410840ca6b46979edac96713718d034051b42
Owen <123762165+Torpoint@users.noreply.github.com>
2026-03-20 08:57:31 +0000

    Add files via upload

---
+++ userclass/c# implementation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class User
+{
+    // Encapsulation (private fields)
+    private string userID;
+    private string name;
+    private string password;
+
+    public User(string userID, string name, string password)
+    {
+        this.userID = userID;
+        this.name = name;
+        this.password = password;
+    }
+
+    public bool Login(string inputID, string inputPassword)
+    {
+        return userID == inputID && password == inputPassword;
+    }
+
+    // Polymorphism (virtual method)
+    public virtual string DisplayRole()
+    {
+        return "User";
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+}
